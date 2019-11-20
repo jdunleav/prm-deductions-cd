db15c8dd32012e783310b8d77fa4baa8fe076817
Phil Evans <phil.evans@thoughtworks.com>
2019-11-20 11:06:57 +0000

    [PRMT-721] Specify node version to install

--- pipelines/deductions-build-images/pipeline.tf
+++ pipelines/deductions-build-images/pipeline.tf
@@ -1,60 +1,60 @@
 resource "aws_codepipeline" "images-pipeline" {
-  name     = "deductions-build-images"
-  role_arn = "${var.codepipeline_generic_role_arn}"
+    name     = "deductions-build-images"
+    role_arn = "${var.codepipeline_generic_role_arn}"
 
-  artifact_store {
-    location = "${var.artifact_bucket}"
-    type     = "S3"
-  }
+    artifact_store {
+      location = "${var.artifact_bucket}"
+      type     = "S3"
+    }
 
-  stage {
-    name = "source"
+    stage {
+      name = "source"
 
-    action {
-      name             = "GithubSource"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = "1"
-      output_artifacts = ["source"]
+      action {
+        name             = "GithubSource"
+        category         = "Source"
+        owner            = "ThirdParty"
+        provider         = "GitHub"
+        version          = "1"
+        output_artifacts = ["source"]
 
-      configuration = {
-        Owner                = "nhsconnect"
-        Repo                 = "prm-deductions-cd"
-        Branch               = "master"
-        OAuthToken           = "${var.github_token_value}"
-        PollForSourceChanges = "true"
+        configuration = {
+          Owner                = "nhsconnect"
+          Repo                 = "prm-deductions-cd"
+          Branch               = "master"
+          OAuthToken           = "${var.github_token_value}"
+          PollForSourceChanges = "true"
+        }
       }
     }
-  }
 
-  stage {
-    name = "build-images"
+    stage {
+        name = "build-images"
+
+        action {
+          name            = "build-terraform-012-image"
+          category        = "Build"
+          owner           = "AWS"
+          provider        = "CodeBuild"
+          version         = "1"
+          input_artifacts = ["source"]
+
+          configuration = {
+            ProjectName = "${aws_codebuild_project.prm-build-terraform-012-image.name}"
+          }
+      }    
 
     action {
-      name            = "build-terraform-012-image"
-      category        = "Build"
-      owner           = "AWS"
-      provider        = "CodeBuild"
-      version         = "1"
-      input_artifacts = ["source"]
+        name            = "build-node-image"
+        category        = "Build"
+        owner           = "AWS"
+        provider        = "CodeBuild"
+        version         = "1"
+        input_artifacts = ["source"]
 
-      configuration = {
-        ProjectName = "${aws_codebuild_project.prm-build-terraform-012-image.name}"
-      }
-    }    
-
-  action {
-      name            = "build-node-image"
-      category        = "Build"
-      owner           = "AWS"
-      provider        = "CodeBuild"
-      version         = "1"
-      input_artifacts = ["source"]
-
-      configuration = {
-        ProjectName = "${aws_codebuild_project.prm-build-node-image.name}"
+        configuration = {
+          ProjectName = "${aws_codebuild_project.prm-build-node-image.name}"
+        }
       }
     }
-  }
 }
